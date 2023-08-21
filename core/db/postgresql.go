eb436fcadc555ce7439d4e035786f3cd6b2eceb8
guilhermegcaires <guilhermegcaires@gmail.com>
2023-08-20 22:38:41 -0300

    [feat] Creation of database tables

--- core/db/postgresql.go
+++ core/db/postgresql.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 const (
@@ -22,19 +23,88 @@ func NewDatabase() (*Postgres, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	dbConnection, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := db.Ping(); err != nil {
+	if err := dbConnection.Ping(); err != nil {
 		return nil, err
 	}
 
-	return &Postgres{Connection: db}, nil
+	return &Postgres{Connection: dbConnection}, nil
 }
 
 // Close closes the database connection
 func (d *Postgres) Close() {
 	d.Connection.Close()
+}
+
+func (d *Postgres) Setup() {
+	err := d.CreateAuthorTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = d.CreateBookTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = d.CreateBookAuthorJunctionTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (d *Postgres) CreateAuthorTable() error {
+	createAuthorTableSQL :=
+		`
+      CREATE TABLE IF NOT EXISTS author (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL
+    )`
+
+	_, err := d.Connection.Exec(createAuthorTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func (d *Postgres) CreateBookTable() error {
+	createBookTableSQL :=
+		`
+      CREATE TABLE IF NOT EXISTS book (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(150) NOT NULL,
+        edition INTEGER NOT NULL,
+        publication_year INTEGER NOT NULL
+      )
+  `
+	_, err := d.Connection.Exec(createBookTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func (d *Postgres) CreateBookAuthorJunctionTable() error {
+	createBookAuthorJunctionSQL :=
+		`
+      CREATE TABLE IF NOT EXISTS author_book (
+        book_id INTEGER REFERENCES book(id),
+        author_id INTEGER REFERENCES author(id),
+        PRIMARY KEY (book_id, author_id)
+      )
+    `
+
+	_, err := d.Connection.Exec(createBookAuthorJunctionSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
--- core/server.go
+++ core/server.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/guilhermegcaires/olist/core/db"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/guilhermegcaires/olist/core/db"
+)
 
 func main() {
 	db, err := db.NewDatabase()
@@ -9,4 +15,26 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+
+	db.Setup()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to the homepage!")
+	})
+
+	r.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "This is the about page.")
+	})
+
+	r.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Contact us at: contact@example.com")
+	})
+
+	port := 8080
+	fmt.Printf("Server is listening on port %d...\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+	if err != nil {
+		panic(err)
+	}
 }
